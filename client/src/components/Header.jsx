0a5360c5ab145f1d49165a6e4c8dffd6ec208edf
ssoura <19mca8281@cuchd.in>
2022-03-29 06:58:21 +0000

    update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
   return (
     <header className='header'>
       <div className='logo'>
-        <Link to='/'>Support Desk</Link>
+        <Link to='/'>Support</Link>
       </div>
       <ul>
         {user ? (
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,15 +6,14 @@ function Home() {
     <>
       <section className='heading'>
         <h1>What do you need help with?</h1>
-        <p>Please choose from an option below</p>
       </section>
 
       <Link to='/new-ticket' className='btn btn-reverse btn-block'>
-        <FaQuestionCircle /> Create New Ticket
+        <FaQuestionCircle /> Create Ticket
       </Link>
 
       <Link to='/tickets' className='btn btn-block'>
-        <FaTicketAlt /> View My Tickets
+        <FaTicketAlt /> View Tickets
       </Link>
     </>
   )
