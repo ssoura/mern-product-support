72f43de20730efdd015e2a06fb0ce5b6e805a934
= <=>
2023-08-31 11:03:20 +0530

    add endpoints

--- server/src/middleware/error.middleware.js
+++ server/src/middleware/error.middleware.js
@@ -1,16 +1,16 @@
-const { logEvents } = require("./logger.middleware");
+const { logEvents } = require('./logger.middleware')
 
 const errorHandler = (err, req, res, next) => {
   logEvents(
     `${err.name}\t${err.name}\t${req.method}\t${req.headers.origin}`,
-    "errLog.log"
-  );
-  const statusCode = res.statusCode ? res.statusCode : 500;
-  res.status(statusCode);
+    'errLog.log'
+  )
+  const statusCode = res.statusCode ? res.statusCode : 500
+  res.status(statusCode)
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
-};
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  })
+}
 
-module.exports = { errorHandler };
+module.exports = { errorHandler }
